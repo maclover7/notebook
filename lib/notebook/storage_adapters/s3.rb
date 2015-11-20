48d24d011b7eac87e3ab977be9f630d5fcc9c774
Jon Moss <me@jonathanmoss.me>
2015-11-20 16:39:02 -0500

    Add #upload to S3 storage adapter

--- lib/notebook/storage_adapters/s3.rb
+++ lib/notebook/storage_adapters/s3.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'securerandom'
 
 module Notebook
   # Return the current S3 options
@@ -13,28 +14,38 @@ module Notebook
 
   module StorageAdapters
     class S3 < Base
+      attr_reader :s3_file_key
+
       def initialize(attachment, options = {})
         super
         @storage_directory = options.fetch('storage_directory', Notebook.public_directory)
         #--
         check_valid_credentials
-        @s3_options = Notebook.s3_options
-        @s3 = Aws::S3::Resource.new(@s3_options)
-        credentials = @s3.client.config.credentials
-        @access_key_id = credentials.access_key_id
-        @bucket_name = bucket
-        @bucket = @s3.bucket(@bucket_name)
+        setup_s3
+      end
+
+      def upload
+        @s3_file_key = "#{SecureRandom.uuid}/#{File.basename(attachment.file)}"
+        @file_object = @bucket.object(@s3_file_key)
+        @file_object.upload_file(attachment.file.path)
       end
 
       private
 
       def check_valid_credentials
         credentials = Notebook.s3_options
-        required_credential_keys = [:access_key_id, :region, :secret_access_key]
+        required_credential_keys = [:access_key_id, :bucket, :region, :secret_access_key]
         contains_credentials = required_credential_keys.all? { |k| credentials.key?(k) }
 
         return if contains_credentials
         fail 'Proper credentials are not defined'
+      end
+
+      def setup_s3
+        @s3_options = Notebook.s3_options
+        @credentials = Aws::Credentials.new(@s3_options[:access_key_id], @s3_options[:secret_access_key])
+        @s3 = Aws::S3::Resource.new(credentials: @credentials, region: @s3_options[:region])
+        @bucket = @s3.bucket(@s3_options[:bucket])
       end
     end
   end
--- spec/storage_adapters/s3_spec.rb
+++ spec/storage_adapters/s3_spec.rb
@@ -15,7 +15,7 @@ describe Notebook::StorageAdapters::S3 do
 
     context 'sufficient hash keys' do
       it 'does not raise error' do
-        s3_options = { access_key_id: 'hi', region: 'hi', secret_access_key: 'hi' }
+        s3_options = { access_key_id: 'hi', bucket: 'hi', region: 'hi', secret_access_key: 'hi' }
         Notebook.s3_options = s3_options
 
         expect do
@@ -26,11 +26,45 @@ describe Notebook::StorageAdapters::S3 do
         )
       end
     end
+
+    describe '#upload' do
+      before do
+        @s3_options = { access_key_id: ENV['NOTEBOOK_AWS_ACCESS_KEY_ID'],
+                        bucket: ENV['NOTEBOOK_AWS_BUCKET'],
+                        region: ENV['NOTEBOOK_AWS_REGION'],
+                        secret_access_key: ENV['NOTEBOOK_AWS_SECRET_ACCESS_KEY'] }
+        Notebook.s3_options = @s3_options
+      end
+
+      it 'returns true' do
+        file = File.open(SpecHelper.fixture_directory + 'avatar.png')
+        attachment = Notebook::Attachment.new(file)
+        adapter = Notebook::StorageAdapters::S3.new(attachment)
+
+        result = adapter.upload
+        expect(result).to eq(true)
+      end
+
+      it 'persists the file' do
+        file = File.open(SpecHelper.fixture_directory + 'avatar.png')
+        attachment = Notebook::Attachment.new(file)
+        adapter = Notebook::StorageAdapters::S3.new(attachment)
+        adapter.upload
+
+        @credentials = Aws::Credentials.new(@s3_options[:access_key_id], @s3_options[:secret_access_key])
+        @s3 = Aws::S3::Resource.new(credentials: @credentials, region: @s3_options[:region])
+        @bucket = @s3.bucket(@s3_options[:bucket])
+
+        result = @bucket.object(adapter.s3_file_key).exists?
+
+        expect(result).to eq(true)
+      end
+    end
   end
 
   describe '#s3_options' do
     it 'sets correctly' do
-      s3_options = { access_key_id: 'hi', region: 'hi', secret_access_key: 'hi' }
+      s3_options = { access_key_id: 'hi', bucket: 'hi', region: 'hi', secret_access_key: 'hi' }
       Notebook.s3_options = s3_options
       expect(Notebook.s3_options).to eq(s3_options)
     end
